df4e4246f83f9c67b5273108ef7a871d55d94adf
Alberto Sanchez <asanchez@talpor.com>
2014-07-03 00:12:34 -0430

    add "first" version of the plugin

---
+++ jquery.fluidSlider.js
@@ -0,0 +1,210 @@
+/**
+ * Plugin Name: fluidSlider
+ * Plugin URI: https://github.com/alb3rto/fluidSlider
+ * Description: A simple slider based in fluid boxes and percentage widths.
+ * Version: 0.1
+ * Author: Alberto Sanchez
+ * Author https://twitter.com/alb3rto269
+ * License: The MIT License (MIT)
+ */
+
+
+/* global jQuery: true */
+;(function($, window, document, undefined) {
+	'use strict';
+
+	var pluginName = 'slider',
+		defaults = {
+            delay: 8000,
+            autoPlay: false,
+            nav:  ''
+        };
+
+	function Slider(element, options) {
+		this.element = element;
+		this.$el = $(element);
+		this.options = $.extend({}, defaults, options);
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init();
+	}
+
+    Slider.prototype = {
+		init: function() {
+            this.$list = this.$el.find('ul');
+            this.$items = this.$list.children();
+            this.size = this.$items.length;
+
+            if(!this.size)
+                return;
+
+            // set elements dimentions according the list size
+            this.$list.css({
+                'visibilily': 'visible', // ? why
+                'width': 100 * this.size + '%'
+            });
+            this.$items.css('width', 100/this.size + '%');
+
+            // init slider nav links
+            this.initNav();
+
+            if(this.options.autoPlay)
+                this.start();
+
+        },
+        start: function(){
+            // begins the slides "loop"
+            if(typeof this.current == 'undefined')
+                this.current = 0;
+
+            this.play = true;
+            this._slide();
+        },
+        stop: function(){
+            // stop the slides "loop"
+            this.play = false;
+            this._clearSchedule();
+        },
+        index: function(newIndex){
+            // Getter & setter for ``current`` property
+            if(typeof newIndex === 'undefined')
+                return this.current;
+
+            if(this.current == newIndex)
+                return;
+
+            // normalize the index before assignement
+            this.current = newIndex % (this.size + 1);
+            this._slide();
+        },
+        prev: function(){
+            // Decrease the current index
+            var current = this.index() || this.size;
+            this.index(current - 1);
+        },
+        next: function(){
+            // Increase the current index
+            var current = this.index();
+            this.index(current + 1);
+        },
+        destroy: function(){
+            // Remove elements, remove data, unregister listerners, etc
+            this._clearSchedule();
+            this.$el.off('.Slider');
+            this.$el.removeData();
+        },
+
+        _slide: function(){
+            // 1. prevent multiple slides
+            this._clearSchedule();
+
+            // 2. preform the slide animation
+            this._animate(this.current);
+
+            // 3. Get the exposure time of the current item and
+            //    schedule the next transition (only if required).
+            if(this.play && this.current != this.size){
+                var delay = $(this.$items[this.current]).data('delay') || this.options.delay;
+                this._sliderProgram = setTimeout($.proxy(this.next, this), delay);
+            }
+        },
+        _clearSchedule: function(){
+            // Clear scheduled transitions
+            clearTimeout(this._sliderProgram);
+        },
+        _animate: function(index){
+            // Performs the transition between the current slide and the next one.
+            var self = this,
+                callback;
+
+            // if the end of the list is reached, restart the slides loop
+            if(index == this.size){
+                callback = function(){
+                    self._reset();
+                    self.index(0);
+                };
+            }
+
+            // Performs the slide (animating the margin left of the list).
+            this.$list.animate({'margin-left': -100 * index + '%'}, {
+                duration: 'slow',
+                // progress: self._slideStep,
+                complete: callback,
+                done: function(){
+                    self._updateNav(index);
+                    self.$el.trigger('updated.Slider', { value: index});
+                }
+            });
+        },
+
+        _slideStep: function(promise, progress){
+            $(promise.elem).css('opacity', Math.abs(0.5-progress)*2);
+        },
+        _reset: function(){
+            // Prepare the slider to show the first slide again
+            this.$list
+                .css('visibility', 'hidden')
+                .css('margin-left', '100%')
+                .css('visibility', 'visible');
+        },
+
+        /* Nav control functions */
+        initNav: function(){
+            if(typeof this.options.nav == 'string' || this.options.nav instanceof Element)
+                this.$navItems = $(this.options.nav).children();
+            else if(this.options.nav instanceof jQuery)
+                this.$navItems = this.options.nav.children();
+
+
+            if(!(this.$navSize = this.$navItems.length))
+                return;
+
+            $.each(this.$navItems, function(i, item){
+                $(item).data('Slider.Nav-index', i);
+            });
+
+            this.$navItems.on('click', $.proxy(this._clickNav, this));
+        },
+        _clickNav: function(e){
+            e.preventDefault();
+
+            // prevent next slide event
+            this._clearSchedule();
+
+            // navigate to the desired index
+            var index = $(e.currentTarget).data('Slider.Nav-index');
+            this.index(index);
+        },
+        _updateNav: function(index){
+            // highlight the navItem related to teh current slide
+            if(index < this.$navSize){
+                this.$navItems.find('a.is-active').removeClass('is-active');
+                $(this.$navItems[index]).find('a').addClass('is-active');
+            }
+        }
+
+	};
+
+
+	$.fn[pluginName] = function(option) {
+		var args = arguments,
+			result;
+
+		this.each(function() {
+			var $this = $(this),
+				data = $.data(this, 'plugin_' + pluginName),
+				options = typeof option === 'object' && option;
+
+            if (!data) {
+				$this.data('plugin_' + pluginName, (data = new Slider(this, options)));
+			}
+
+			if (typeof option === 'string' && options.length && options[0] != '_') {
+				result = data[option].apply(data, Array.prototype.slice.call(args, 1));
+			}
+		});
+
+        return typeof result === 'undefined' ? this : result;
+	};
+
+})(jQuery, window, document);
